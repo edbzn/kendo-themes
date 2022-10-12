f74e708fec1b084c3859627c419f92a17b16a7b5
Ivo Valkov <ivos.valkov@gmail.com>
2022-10-12 17:05:40 +0300

    test(toolbar): use toolbar html helper in visual tests

--- packages/html/src/scheduler/tests/scheduler-react-timeline.tsx
+++ packages/html/src/scheduler/tests/scheduler-react-timeline.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Button } from '../../button';
 import { Icon } from '../../icon';
+import { Toolbar } from '../../toolbar';
 
 const root = ReactDOM.createRoot(
     document.getElementById('app') as HTMLElement
@@ -18,8 +19,8 @@ root.render(
     <>
         <style>{style}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-1">
-            <div className="k-widget k-scheduler k-scheduler-flex" style={{ height: "400px" }} >
-                <div className="k-scheduler-toolbar k-toolbar">
+            <div className="k-widget k-scheduler k-scheduler-flex" style={{ height: "900px" }} >
+                <Toolbar className="k-scheduler-toolbar" resizable={false}>
                     <span className="k-button-group k-scheduler-navigation">
                         <Button>Today</Button>
                         <Button icon="caret-alt-left"></Button>
@@ -41,7 +42,7 @@ root.render(
                             <Button>Agenda</Button>
                         </span>
                     </span>
-                </div>
+                </Toolbar>
                 <div className="k-scheduler-layout k-scheduler-layout-flex k-scheduler-timeline-view">
                     <div className="k-scheduler-head" style={{ width: "7201px" }} >
                         <div className="k-scheduler-group k-group-horizontal">
@@ -442,9 +443,9 @@ root.render(
                         </div>
                     </div>
                 </div>
-                <div className="k-scheduler-footer k-toolbar">
+                <Toolbar className="k-scheduler-footer" resizable={false}>
                     <Button icon="clock" className="k-scheduler-fullday">Show full day</Button>
-                </div>
+                </Toolbar>
             </div>
             <span></span>
 
